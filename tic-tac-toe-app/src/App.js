aa5808c4e3ac085f65be2f1a082a00afc60119d3
Lamia Jabin Rimty <rimtycse@gmail.com>
2024-07-17 11:43:04 +0200

    state management by main charkona component

--- tic-tac-toe-app/src/App.js
+++ tic-tac-toe-app/src/App.js
@@ -15,27 +15,27 @@ function Choturvuj({value,onChoturvujClick}){
 function Charkona() {
   const [choturvuj,setChoturvuj] = useState(Array(9).fill(null));
 
- function handleClick(){
+ function handleClick(i){
     const nextChoturvuj = choturvuj.slice();
-    nextChoturvuj[0]="x";
+    nextChoturvuj[i]="x";
     setChoturvuj(nextChoturvuj);
  }
   return (
     <>
       <div className="charkona-row">
-     <Choturvuj  value={choturvuj[0]} onChoturvujClick={handleClick}/>
-     <Choturvuj value={choturvuj[1]} onChoturvujClick={handleClick}/>
-     <Choturvuj value={choturvuj[2]} onChoturvujClick={handleClick}/>
+     <Choturvuj  value={choturvuj[0]} onChoturvujClick={()=>handleClick(0)}/>
+     <Choturvuj value={choturvuj[1]} onChoturvujClick={()=>handleClick(1)}/>
+     <Choturvuj value={choturvuj[2]} onChoturvujClick={()=>handleClick(2)}/>
       </div>
       <div className="charkona-row">
-      <Choturvuj value={choturvuj[3]} onChoturvujClick={handleClick}/> 
-      <Choturvuj value={choturvuj[4]} onChoturvujClick={handleClick}/>
-      <Choturvuj value={choturvuj[5]} onChoturvujClick={handleClick}/>
+      <Choturvuj value={choturvuj[3]} onChoturvujClick={()=>handleClick(3)}/> 
+      <Choturvuj value={choturvuj[4]} onChoturvujClick={()=>handleClick(4)}/>
+      <Choturvuj value={choturvuj[5]} onChoturvujClick={()=>handleClick(5)}/>
       </div>
       <div className="charkona-row">
-      <Choturvuj value={choturvuj[6]} onChoturvujClick={handleClick}/>
-      <Choturvuj value={choturvuj[7]} onChoturvujClick={handleClick}/>
-      <Choturvuj value={choturvuj[8]} onChoturvujClick={handleClick}/>
+      <Choturvuj value={choturvuj[6]} onChoturvujClick={()=>handleClick(6)}/>
+      <Choturvuj value={choturvuj[7]} onChoturvujClick={()=>handleClick(7)}/>
+      <Choturvuj value={choturvuj[8]} onChoturvujClick={()=>handleClick(8)}/>
       </div>
     </>
   );
