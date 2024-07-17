3fba0ac42b5f59633cd97c75b46c452e7c4dfc9c
Lamia Jabin Rimty <rimtycse@gmail.com>
2024-07-17 11:21:34 +0200

    receive any value prop

--- tic-tac-toe-app/src/App.js
+++ tic-tac-toe-app/src/App.js
@@ -1,35 +1,41 @@
 import { useState } from "react";
 
-function Choturvuj(){
-const [value,setValue]=useState(null);
+function Choturvuj({value,onChoturvujClick}){
 
-  function handleClick(){
-    setValue("x");
-  }
+  // function handleClick(){
+  //   setChoturvuj("x");
+  // }
   return( 
   <button 
   className="choturbuj" 
-  onClick={handleClick}>{value}</button>
+  onClick={onChoturvujClick}>{value}</button>
 )
 }
 
 function Charkona() {
+  const [choturvuj,setChoturvuj] = useState(Array(9).fill(null));
+
+ function handleClick(){
+    const nextChoturvuj = choturvuj.slice();
+    nextChoturvuj[0]="x";
+    setChoturvuj(nextChoturvuj);
+ }
   return (
     <>
       <div className="charkona-row">
-     <Choturvuj />
-     <Choturvuj />
-     <Choturvuj />
+     <Choturvuj  value={choturvuj[0]} onChoturvujClick={handleClick}/>
+     <Choturvuj value={choturvuj[1]} onChoturvujClick={handleClick}/>
+     <Choturvuj value={choturvuj[2]} onChoturvujClick={handleClick}/>
       </div>
       <div className="charkona-row">
-      <Choturvuj /> 
-      <Choturvuj />
-      <Choturvuj />
+      <Choturvuj value={choturvuj[3]} onChoturvujClick={handleClick}/> 
+      <Choturvuj value={choturvuj[4]} onChoturvujClick={handleClick}/>
+      <Choturvuj value={choturvuj[5]} onChoturvujClick={handleClick}/>
       </div>
       <div className="charkona-row">
-      <Choturvuj />
-      <Choturvuj />
-      <Choturvuj />
+      <Choturvuj value={choturvuj[6]} onChoturvujClick={handleClick}/>
+      <Choturvuj value={choturvuj[7]} onChoturvujClick={handleClick}/>
+      <Choturvuj value={choturvuj[8]} onChoturvujClick={handleClick}/>
       </div>
     </>
   );
