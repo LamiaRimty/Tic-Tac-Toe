da25c176b5cc47dd7e8a74fb4563aefbec9ec0ab
Lamia Jabin Rimty <rimtycse@gmail.com>
2024-07-18 12:45:42 +0200

    calculation of winner

--- tic-tac-toe-app/src/App.js
+++ tic-tac-toe-app/src/App.js
@@ -49,4 +49,25 @@ function Charkona() {
   );
 }
 
+function calculateWinner(choturbuj){
+  const lines =[
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
+  for(let i=0; i<lines.length;i++){
+    const [a,b,c]= lines[i];
+    if(choturbuj[a] && choturbuj[a] === choturbuj[b] && choturbuj[b] === choturbuj[c] &&  choturbuj[c]){
+      return choturbuj[a];
+    }
+  }
+  return null;
+}
+
 export default Charkona;
